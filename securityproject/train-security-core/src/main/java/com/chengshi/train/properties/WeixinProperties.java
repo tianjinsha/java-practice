605e0d89bde2cf779c2fb9bce52b8da702bcb69b
tianjinsha <18780183730@163.com>
2019-01-30 09:26:00 +0800

    修改qq无法登陆到登陆页面

--- securityproject/train-security-core/src/main/java/com/chengshi/train/properties/WeixinProperties.java
+++ securityproject/train-security-core/src/main/java/com/chengshi/train/properties/WeixinProperties.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.social.SocialProperties;
 /**
  * 微信属性
  */
-public class WeixinProperties extends SocialProperties {
+public class QQProperties extends SocialProperties {
     /**
-     * 第三方id，用来决定发起第三方登录的url，默认是 weixin。
+     * 第三方id，用来决定发起第三方登录的url，默认是 qq。
      */
-    private String providerId = "weixin";
+    private String providerId = "qq";
 
     public String getProviderId() {
         return providerId;
---
+++ securityproject/train-security-core/src/main/java/com/chengshi/train/properties/aiqqProperties.java
@@ -0,0 +1,7 @@
+package com.chengshi.train.properties;
+
+import org.springframework.boot.autoconfigure.social.SocialProperties;
+
+public class aiqqProperties extends SocialProperties {
+    private String   providerId="qq";
+}
