7204321616428fe585ed90d748b0b9926692d8ad
tian <42697470+tianjinsha@users.noreply.github.com>
2019-12-11 14:13:08 +0800

    add hie

---
+++ hadoop/hive/src/test/java/com/train/hive/HiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
