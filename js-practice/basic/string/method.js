3deb7946a36ddf45d838975d13757d6edb6a0466
tianjinsha <18780183730@163.com>
2020-12-26 12:17:24 +0800

    添加vue-cli3

---
+++ js-practice/basic/string/method.js
@@ -0,0 +1,27 @@
+
+/**
+ * slice 
+ * stringObject.slice(start,end)
+ */
+
+let a ='abcdefghijklmn'
+let b
+b = a.slice(1,3)
+console.log(b)
+
+
+/**
+ * substr
+ * stringObject.substr(start,length)
+ */
+
+ b = a.substr(1,3)
+ console.log(b)
+
+ /**
+  * substring
+  * stringObject.substring(start,stop)
+  */
+
+  b = a.substring(1,3)
+  console.log(b)
